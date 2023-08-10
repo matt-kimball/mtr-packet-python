754c761cd1780cfe75a75c3f01d87b3a3edb9478
Matt Kimball <matt.kimball@gmail.com>
2023-08-10 13:10:46 -0600

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mtrpacket",
-    version="1.0.0",
+    version="1.0.1",
     python_requires=">=3.5",
     author="Matt Kimball",
     author_email="matt.kimball@gmail.com",
